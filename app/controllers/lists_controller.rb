79e03e90fa9b319361a8e626956ee8d43d654f9c
HikaruSato <110364092+Omochi800@users.noreply.github.com>
2022-08-11 00:56:42 +0000

    [Finish]4-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -32,7 +32,7 @@ class ListsController < ApplicationController
   end
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :i)
   end  
   
 end
